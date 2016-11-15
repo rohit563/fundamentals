8ff7162560389cf488fa1f6db975fa4d9a71c174
Rohit Banda <rohit-banda@uiowa.edu>
2016-11-15 21:56:47 +0000

    push

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Profile</div>
                 <div class="panel-body">
-                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/profile', Auth::user()->id) }}">
+                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/profile') }}">
                         {{ csrf_field() }}
                         <input type="hidden" name="_method" value="PUT">
     
